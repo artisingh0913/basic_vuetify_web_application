0d4c1b4fb29fec7e999709501745ccb60f8d68f0
Arti Singh <49314856+artisingh0913@users.noreply.github.com>
2020-08-04 15:35:39 -0400

    Base Web Code using Vuetify

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>    
+    <Navg/>
+    <!-- <v-app-bar/> -->
+    <v-divider></v-divider>
+    <v-content>      
+      <v-container fill-height align-center> 
+        <v-row
+        justify="center"
+        no-gutters
+        >         
+          <!-- align-center justify-center -->
+          <router-view/>   
+        </v-row>     
+      </v-container>
+    </v-content>
+    <!-- <v-footer app> -->
+    <Footer/>
+  <!-- </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+import Navg from './components/Navg';
+import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+
+  components: {
+    Navg,
+    Footer
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
